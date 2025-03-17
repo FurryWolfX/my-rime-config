a92c170ff373f99730732c43df1183f38e93a3ed
wolfx <digimonx@qq.com>
2025-03-17 20:50:34 +0800

    超越输入法打包脚本

---
+++ other_dist_config/beyond/build-schema.bat
@@ -0,0 +1,5 @@
+7za a -tzip -mx=0 schema_wolfx.zip E:\rime\ -xr!.git -xr!*.userdb -xr!.* -xr!other_dist_config -xr!build -xr!doc
+
+hdc file send .\schema_wolfx.zip /storage/media/100/local/files/Docs/Download
+
+pause
