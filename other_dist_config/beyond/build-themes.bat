79fd01a7b79e828d04254391d85656b01d7a8a87
wolfx <digimonx@qq.com>
2025-03-18 21:23:04 +0800

    主题更新

--- other_dist_config/beyond/build-themes.bat
+++ other_dist_config/beyond/build-themes.bat
@@ -1,7 +1,4 @@
 7za a -tzip -mx=0 themes_wolfx_pad.zip E:\rime\other_dist_config\beyond\themes\wolfx_pad -xr!.git
-7za a -tzip -mx=0 themes_wolfx_pad_split.zip E:\rime\other_dist_config\beyond\themes\wolfx_pad_split -xr!.git
-
 hdc file send .\themes_wolfx_pad.zip /storage/media/100/local/files/Docs/Download
-hdc file send .\themes_wolfx_pad_split.zip /storage/media/100/local/files/Docs/Download
 
 pause
