874b08e09c4d2b28d8a1d4dec8ad50ff95f1ac37
wolfx <digimonx@qq.com>
2025-03-21 22:04:44 +0800

    自用助记

--- other_dist_config/beyond/build-schema.bat
+++ other_dist_config/beyond/build-schema.bat
@@ -1,4 +1,4 @@
-7za a -tzip -mx=0 schema_wolfx.zip E:\rime\ -xr!.git -xr!*.userdb -xr!.* -xr!other_dist_config -xr!build -xr!doc
+7za a -tzip -mx=5 schema_wolfx.zip E:\rime\ -xr!.git -xr!*.userdb -xr!.* -xr!other_dist_config -xr!doc
 
 hdc file send .\schema_wolfx.zip /storage/media/100/local/files/Docs/Download
 
